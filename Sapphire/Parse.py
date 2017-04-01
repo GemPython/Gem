df0a9e11310b414948b870cd445923e98d0571ea
Amit Green <amit.mixie@gmail.com>
2017-04-01 11:12:08 -0400

    Start parsing empty lines, empty comment & comments

--- Sapphire/Parse.py
+++ Sapphire/Parse.py
@@ -3,7 +3,10 @@
 #
 @gem('Sapphire.Parse')
 def gem():
-    class UnknownLine(Object):
+    require_gem('Sapphire.Match')
+
+
+    class Token(Object):
         __slots__ = ((
             's',
         ))
@@ -14,7 +17,35 @@ def gem():
 
 
         def __repr__(t):
-            return arrange('<UnknownLine %s>', portray_string(t.s))
+            return arrange('<%s %s>', t.__class__.__name__, portray_string(t.s))
+
+
+    class Comment(Token):
+        __slots__ = (())
+
+
+
+    class EmptyLine(Token):
+        __slots__ = ((
+            'portray',                  #   String
+        ))
+
+
+        def __init__(t, portray, s):
+            t.portray = portray
+            t.s       = s
+
+
+        def __repr__(t):
+            return arrange('<%s>', t.portray)
+
+
+    empty_line    = EmptyLine('EmptyLine', '')
+    empty_comment = EmptyLine('EmptyComment', '#')
+
+
+    class UnknownLine(Token):
+        pass
 
 
     @share
@@ -24,6 +55,23 @@ def gem():
         append_line = many.append
 
         for s in data.splitlines():
+            m = comment_match(s)
+
+            if m is not none:
+                comment = m.group('comment')
+
+                if comment is not none:
+                    if comment == '':
+                        append_line(empty_comment)
+                        continue
+
+
+                    append_line(Comment(comment))
+                    continue
+
+                append_line(empty_line)
+                continue
+
             append_line(UnknownLine(s))
 
         for v in many:
