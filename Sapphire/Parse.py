5653c82da65b595132b3a192ba9ccb285ac20824
Amit Green <amit.mixie@gmail.com>
2017-04-01 10:47:50 -0400

    Start parsing lines of Gem code

--- Sapphire/Parse.py
+++ Sapphire/Parse.py
@@ -3,9 +3,28 @@
 #
 @gem('Sapphire.Parse')
 def gem():
+    class UnknownLine(Object):
+        __slots__ = ((
+            's',
+        ))
+
+
+        def __init__(t, s):
+            t.s = s
+
+
+        def __repr__(t):
+            return arrange('<UnknownLine %s>', portray_string(t.s))
+
+
     @share
     def parse_python_from_path(path):
-        data = read_text_from_path('../Sapphire/Main.py')
+        data        = read_text_from_path('../Sapphire/Main.py')
+        many        = []
+        append_line = many.append
 
         for s in data.splitlines():
-            line(s)
+            append_line(UnknownLine(s))
+
+        for v in many:
+            line('%r', v)
--- Sapphire/Pattern.py
+++ Sapphire/Pattern.py
@@ -8,11 +8,11 @@ def gem():
     require_gem('Tremolite.CreateMatch')
 
 
-    from Tremolite import create_match_code, DOT, END_OF_PATTERN, MATCH, ZERO_OR_MORE
+    from Tremolite import create_match_code, DOT, END_OF_PATTERN, MATCH, OPTIONAL_GROUP, ZERO_OR_MORE
 
 
 
     @share
     def create_sapphire_match():
-        MATCH('comment_match', '#' + ZERO_OR_MORE(DOT) + END_OF_PATTERN)
+        MATCH('comment_match', OPTIONAL_GROUP('comment', '#') + ZERO_OR_MORE(DOT) + END_OF_PATTERN)
         create_match_code('../Sapphire/Match.py', '2017 Amit Green', 'Sapphire.Match')
