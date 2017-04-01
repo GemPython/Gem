de52c34ba1a235500f305e5c838efa923b61198e
Amit Green <amit.mixie@gmail.com>
2017-04-01 17:35:48 -0400

    Start parsing a define header

--- Sapphire/Parse.py
+++ Sapphire/Parse.py
@@ -24,6 +24,32 @@ def gem():
         __slots__ = (())
 
 
+    class DefineHeader(Token):
+        __slots__ = ((
+            'keyword_define',               #   String
+            'name',                         #   String
+            'left_parenthesis',             #   String
+            'argument_1',                   #   String
+            'right_parenthesis__colon',     #   String
+        ))
+
+
+        def __init__(t, keyword_define, name, left_parenthesis, argument_1, right_parenthesis__colon):
+            t.keyword_define           = keyword_define
+            t.name                     = name
+            t.left_parenthesis         = left_parenthesis
+            t.argument_1               = argument_1
+            t.right_parenthesis__colon = right_parenthesis__colon
+
+
+        def  __repr__(t):
+            return arrange('<DefineHeader %s %s %s %s %s>',
+                           portray_string(t.keyword_define),
+                           t.name,
+                           portray_string(t.left_parenthesis),
+                           t.argument_1,
+                           portray_string(t.right_parenthesis__colon))
+
 
     class EmptyLine(Token):
         __slots__ = ((
@@ -65,9 +91,22 @@ def gem():
                         append_line(empty_comment)
                         continue
 
-
                     append_line(Comment(comment))
                     continue
+
+                keyword_define= m.group('keyword_define')
+
+                if keyword_define is not none:
+                    [
+                        name_1, left_parenthesis, name_2, right_parenthesis__colon,
+                    ] = m.group('name_1', 'left_parenthesis', 'name_2', 'right_parenthesis__colon')
+
+                    append_line(
+                        DefineHeader(keyword_define, name_1, left_parenthesis, name_2, right_parenthesis__colon),
+                    )
+
+                    continue
+
 
                 append_line(empty_line)
                 continue
--- Sapphire/Pattern.py
+++ Sapphire/Pattern.py
@@ -6,13 +6,31 @@ def gem():
     require_gem('Sapphire.Core')
     require_gem('Tremolite.Build')
     require_gem('Tremolite.CreateMatch')
+    require_gem('Tremolite.Name')
 
 
-    from Tremolite import create_match_code, DOT, END_OF_PATTERN, GROUP, MATCH, OPTIONAL, OPTIONAL_GROUP, ZERO_OR_MORE
-
+    from Tremolite import create_match_code, ANY_OF, DOT, EMPTY, FULL_MATCH, GROUP, NAME, NAMED_GROUP
+    from Tremolite import ONE_OR_MORE, OPTIONAL, OPTIONAL_GROUP, ZERO_OR_MORE
 
 
     @share
     def create_sapphire_match():
-        MATCH('comment_match', OPTIONAL('#' + GROUP('comment', ZERO_OR_MORE(DOT))) + END_OF_PATTERN)
+        ow = ZERO_OR_MORE(' ')
+        w  = ONE_OR_MORE(' ')
+        w1 = NAMED_GROUP('w1', w)
+        w2 = NAMED_GROUP('w2', w)
+
+        identifier = NAME('identifier', ANY_OF('A-Z', '_', 'a-z') + ZERO_OR_MORE(ANY_OF('0-9', 'A-Z', '_', 'a-z')))
+        name_1     = NAMED_GROUP('name_1', identifier)
+        name_2     = NAMED_GROUP('name_2', identifier)
+
+        left_parenthesis         = NAMED_GROUP('left_parenthesis', ow + '(' + ow)
+        right_parenthesis__colon = NAMED_GROUP('right_parenthesis__colon', ow + ')' + ow + ':' + ow)
+        keyword_define           = NAMED_GROUP('keyword_define', 'def' + w)
+
+        comment  = NAME('comment', '#' + GROUP('comment', ZERO_OR_MORE(DOT)))
+        define_1 = NAME('define_1', keyword_define + name_1 + left_parenthesis + name_2 + right_parenthesis__colon)
+
+        FULL_MATCH('comment_match', comment | define_1 | EMPTY)
+ 
         create_match_code('../Sapphire/Match.py', '2017 Amit Green', 'Sapphire.Match')
