c95bc69a2a4ff174a3c68ecbdbe4ed226ec75454
Amit Green <amit.mixie@gmail.com>
2017-04-02 04:25:37 -0400

    Rewrite produce_cache_and_insert_function to produce_cache_functions (and implement produce_conjure & produce_lookup)

--- Gem/Cache.py
+++ Gem/Cache.py
@@ -3,32 +3,54 @@
 #
 @gem('Gem.Cache')
 def gem():
-    if __debug__:
-        @export
-        def produce_cache_and_insert_function(name):
-            cache    = {}
+    @export
+    def produce_cache_functions(
+            name,
+            produce_cache   = false,
+            produce_conjure = false,
+            produce_insert  = false,
+            produce_lookup  = false,
+    ):
+        result = []
+        append = result.append
+
+        cache = {}
+
+        if (produce_conjure) or (produce_insert):
+            provide = cache.setdefault
+
+        if (produce_conjure) or (produce_lookup):
+            lookup = cache.get
+
+        if produce_cache:
+            append(cache)
+
+        if produce_conjure:
+            def conjure(k, meta):
+                return (lookup(k)) or (provide(k, meta(k)))
+
+
+            append(conjure)
+
+        if produce_insert:
             contains = cache.__contains__
             find     = cache.__getitem__
-            provide  = cache.setdefault
-
-            def insert(k, v):
-                if contains(k):
-                    raise_runtime_error('cache %s: attempt to insert key %r with value %r (already has value %r)',
-                                        name, k, v, find(k))
-
-                return provide(k, v)
 
 
-            return ((
-                       cache,
-                       insert,
-                   ))
-    else:
-        @export
-        def produce_cache_and_insert_function(name):
-            cache = {}
+            if __debug__:
+                def insert(k, v):
+                    if contains(k):
+                        raise_runtime_error('cache %s: attempt to insert key %r with value %r (already has value %r)',
+                                            name, k, v, find(k))
 
-            return ((
-                       cache,
-                       cache.setdefault,
-                   ))
+                    return provide(k, v)
+
+
+                append(insert)
+            else:
+                append(provide)
+
+        if produce_lookup:
+            append(lookup)
+
+        return Tuple(result)
